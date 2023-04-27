5aff898193a5a9127c81101afc996996f569ef86
mustythefirst <m.adde.osman@gmail.com>
2023-04-27 14:59:59 +0100

    https://github.com/mustythefirst/local-hardhat-games.git

--- scripts/win.js
+++ scripts/win.js
@@ -1,16 +1,25 @@
 // add the game address here and update the contract name if necessary
-const gameAddr = "";
-const contractName = "Game1";
+const gameAddr = "0x7a2088a1bFc9d81c55368AE168C2C02570cB814F";
+const contractName = "Game2";
 
 async function main() {
     // attach to the game
     const game = await hre.ethers.getContractAt(contractName, gameAddr);
 
     // do whatever you need to do to win the game here:
-    const tx = await game.win();
+    const tx1 = await game.setX(40);
+    await tx1.wait();
+    
+    console.log(tx1);
+
+    const tx2 = await game.setY(10);
+    await tx2.wait();
+
+    console.log(tx2);
 
     // did you win? Check the transaction receipt!
     // if you did, it will be in both the logs and events array
+    const tx = await game.win();
     const receipt = await tx.wait();
     console.log(receipt);
 }
